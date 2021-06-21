bd5d8667098af1eef4375ac75e073ea46b10d586
William Phokompe <phokompe.william@outlook.com>
2021-06-21 13:02:09 +0200

    🎨

---
+++ TravelTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TravelTime
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
