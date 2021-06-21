3075af77b7a075a67292311b6d877cf5984a5877
William Phokompe <phokompe.william@outlook.com>
2021-06-21 12:47:19 +0200

    🔥

---
+++ travel-time/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace travel_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
